a70560f05b4ed9f05e5f5b4b1fbd659d8d4071eb
KMRSKPHALGUN <phalgunkothamasu@gmail.com>
2024-04-17 18:53:04 +0530

    Now user cannot go to any page from homepage if he is not logged in, and we will be able to know which property has been posted by which user.

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
 const path = require("path");
+//
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+//
+
+const Signup = require('./models/Signup');
 const authController = require('./controllers/authController');
 const ResidentialRent = require('./controllers/Residential_rent');
 const ResidentialSale = require('./controllers/Residential_sale');
@@ -18,6 +26,38 @@ const port = 6001;
 
 mongoose.connect('mongodb://localhost:27017/DreamSpaces');
 
+//
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }));
+//
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Configure Passport with local strategy
+passport.use('local',new LocalStrategy({usernameField:"l_email",passwordField:"l_password"},
+  async function (l_email, l_password, done,err) {
+    // Find user by username and verify password
+    const user = await Signup.findOne({ email: l_email });
+      //if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      const match = await bcrypt.compare(l_password, user.password);
+      if (!match) { return done(null, false); }
+      return done(null, user);
+  }
+));
+
+// Serialize user into session
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+// Deserialize user from session
+passport.deserializeUser(async function(id, done) {
+    const user =  await Signup.findOne({_id: id});
+    done(null, user);
+
+});
+
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -27,7 +67,7 @@ app.use(bodyParser.json());
 
 
 // Routes for login and registration
-app.post('/login', authController.login);
+app.post('/login',function(req,res,next){ next();} ,passport.authenticate('local', { successRedirect: '/index', failureRedirect: '/login' }));
 app.post('/register', authController.register);
 app.post('/residential_rent', ResidentialRent.residentialRent);
 app.post('/residential_sale', ResidentialSale.residentialSale);
@@ -53,22 +93,22 @@ app.get('/property_details', (req, res) => {
 app.get('/index', (req, res) => {
     res.render('index');
 });
-app.get('/in_dex', (req, res) => {
+app.get('/in_dex', isAuthenticated, (req, res) => {
     res.render('property_listings', { property: '' });
 })
-app.get('/post_your_property', (req, res) => {
+app.get('/post_your_property', isAuthenticated, (req, res) => {
     res.render('post_your_property');
 });
-app.get('/notification', (req, res) => {
+app.get('/notification', isAuthenticated, (req, res) => {
     res.render('notification');
 });
 app.get('/message', (req, res) => {
     res.render('message');
 });
-app.get('/user_details', (req, res) => {
+app.get('/user_details', isAuthenticated, (req, res) => {
     res.render('user_details');
 });
-app.get('/login_signup', (req, res) => {
+app.get('/login', (req, res) => {
     res.render('login_signup');
 });
 app.get('/commercial_rent', (req, res) => {
@@ -98,6 +138,14 @@ app.get('/plot_sale', (req, res) => {
 app.get('/error', (req, res) => {
     res.render('error', { error: 'Something went wrong!' });
 });
+
+function isAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect('/login');
+}
+
 app.listen(port, ()=>{
     console.log(`Server is running on http://localhost:${port}`);
 })
--- controllers/Property_Details.js
+++ controllers/Property_Details.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 const sale = require('../models/Residential_sale');
-
+const seller = require('../models/Signup');
 exports.property_details = async (req,res) =>{
     try{
         const object_id = req.body.object_id.toString();
-    
         const property = await sale.findOne({_id: object_id});
+        const owner = await seller.findOne({_id: property.ownerId});
         if(property){
-            res.render('property_details', { property: property });
+            res.render('property_details', { property: property, owner: owner, });
         }
         else {
             res.render('property_details', { property: '' });
